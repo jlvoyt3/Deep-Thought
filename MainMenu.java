62ab3e56160ddbb42f490c111f04215680592a23
haileydenys <47611666+haileydenys@users.noreply.github.com>
2019-03-05 19:04:46 -0500

    Add files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -3,6 +3,7 @@
 
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
@@ -13,19 +14,25 @@ import javafx.stage.Stage;
 
 public class MainMenu extends Pane {
 
-	// buttons
+		//attributes
+	private Stage stage = new Stage();
+	private Pane pane = new Pane();
+	private BorderPane borderpane = new BorderPane();
+	private Pane optionPane = new Pane();
+	private BorderPane optionBorderPane = new BorderPane();	
+	Scene optionScene = new Scene(optionBorderPane, 600, 500);	
+	private Text text = new Text();	
+	
+		// buttons
 	private Button newGameButton = new Button("Start Game");
 	private Button helpButton = new Button("Instructions");
-	private Button optionsButton = new Button("Options");
 	private Button scoreButton = new Button("Top Scores");
-	private Text text= new Text();
-	
-	private Stage stage = new Stage();
-	private BorderPane optionBorderPane = new BorderPane();	
-	private Pane optionPane = new Pane();
-	Scene optionScene = new Scene(optionBorderPane, 1500, 800);
+
+
 	
 	public MainMenu() {
+		
+		
 		newGameButton.setLayoutX(350);
 		newGameButton.setLayoutY(200);
 		newGameButton.setFont( new Font(24));
@@ -33,10 +40,6 @@ public class MainMenu extends Pane {
 		helpButton.setLayoutX(350);
 		helpButton.setLayoutY(400);
 		helpButton.setFont( new Font(24));
-		
-		optionsButton.setLayoutX(365);
-		optionsButton.setLayoutY(300);
-		optionsButton.setFont( new Font(24));
 		
 		scoreButton.setLayoutX(355);
 		scoreButton.setLayoutY(500);
@@ -54,19 +57,17 @@ public class MainMenu extends Pane {
 
 		getChildren().add(newGameButton);
 		getChildren().add(helpButton);
-		getChildren().add(optionsButton);
 		getChildren().add(scoreButton);
 		getChildren().add(text);
 
-		optionPane.setCenter(pane);
+		borderpane.setCenter(pane);
 		optionBorderPane.setCenter(optionPane);
 		
-	//	borderpane.setCenter(pane);
-	//  gameBorderPane.setCenter(gamePane);
 		newGameButton.setOnAction(e-> {
 			
 			stage.setScene(optionScene);
 			stage.show();
+			
 		});
 		
 		helpButton.setOnAction(e-> {
@@ -76,5 +77,70 @@ public class MainMenu extends Pane {
 		scoreButton.setOnAction(e-> {
 			new ScorePage();
 		});
+		
+		Text title = new Text();
+		
+		title.setText("Options");
+		title.setLayoutX(200);
+		title.setLayoutY(150);
+		title.setFont( new Font(60));
+		title.setFill(Color.WHITE);
+		
+		Button backButton = new Button();
+		
+		backButton.setText("Back");
+		backButton.setFont( new Font(24) );
+		backButton.setLayoutX(200);
+		backButton.setLayoutY(400);
+		
+		Button startButton = new Button();
+		
+		startButton.setText("Start Game");
+		startButton.setLayoutX(350);
+		startButton.setLayoutY(400);
+		startButton.setFont( new Font(24));
+		
+		final ComboBox<String> pickPlayers = new ComboBox<String>();
+		pickPlayers.getItems().addAll("Player VS AI","Player VS Player","AI VS AI");
+		pickPlayers.setLayoutX(50);
+		pickPlayers.setLayoutY(300);
+		pickPlayers.setPromptText("Choose Game Type");
+
+		
+		final ComboBox<String> pickDifficulty = new ComboBox<String>();
+		pickDifficulty.getItems().addAll("Easy","Medium","Hard");
+		pickDifficulty.setLayoutX(250);
+		pickDifficulty.setLayoutY(300);
+		pickDifficulty.setPromptText("Choose Difficulty");
+		
+		final ComboBox<String> pickAI = new ComboBox<String>();
+		pickAI.getItems().addAll("AI Jerry","AI Fred","AI Chad");
+		pickAI.setLayoutX(450);
+		pickAI.setLayoutY(300);
+		pickAI.setPromptText("Choose AI");
+		
+		optionPane.getChildren().add(pickPlayers);
+		optionPane.getChildren().add(pickDifficulty);
+		optionPane.getChildren().add(pickAI);
+		optionPane.getChildren().add(title);
+		optionPane.getChildren().add(startButton);
+//		optionPane.getChildren().add(backButton);
+		optionBorderPane.setStyle("-fx-background: #161D61");
+		
+		
+		Pane gamePane = new Pane();
+		BorderPane gameBorderPane = new BorderPane();
+		gameBorderPane.setCenter(gamePane);
+		Scene gameScene =  new Scene(gameBorderPane, 1000, 850);
+		
+		startButton.setOnAction(e->{
+			stage.setScene(gameScene);
+		});
+		
+//		backButton.setOnAction(e->{
+//			stage.setScene(optionScene);
+//		});
+		
+
 	}
 }
