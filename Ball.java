424911e3ae599130034c9ba4c5f9385c3c2cadc3
ERAU2429363 <47841126+ERAU2429363@users.noreply.github.com>
2019-03-05 19:11:15 -0500

    Complete Ball
    This should have completed all the ball methods.. might modify for when attaching to the final pane

--- Ball.java
+++ Ball.java
@@ -1,43 +1,48 @@
-package UML_Diagram;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
+import javax.swing.JOptionPane;
 
-/**
- * @author willd
- * @version 1.0
- * @created 13-Feb-2019 4:08:00 PM
- */
 public class Ball {
+    private static final int width = 20, height = 20;
+    private Pong game;
+    private int x, y, xMovement = 2, yMovement = 2;
 
-	public Manager m_Manager;
+    public Ball(Pong game) {
+        this.game = game;
+        x = game.getWidth() / 2;
+        y = game.getHeight() / 2;
+    }
 
-	public Ball(){
+    public void update() {
+        x = x + xMovement;
+        y = y + yMovement;
+        if (x < 0) {												
+            game.getPanel().increaseScore(1);
+            x = game.getWidth() / 2;
+            xMovement = -xMovement;
+        }
+        else if (x > game.getWidth() - width - 10) {				//if the ball scores on player2
+            game.getPanel().increaseScore(2);						//increase score of player
+            x = game.getWidth() / 2;
+            xMovement = -xMovement;									//reflect x axis movement
+        }
+        else if (y < 0 || y > game.getHeight() - height - 35)		//if the ball hit the "roof" is reflects along y axis
+            yMovement = -yMovement;
+        
+        Collision();
+    }
 
-	}
+    public void Collision() {										// if collision reflect along the x axis for movement
+        if (game.getPanel().getPlayer(1).getBounds().intersects(getPosition()) || game.getPanel().getPlayer(2).getBounds().intersects(getPosition()))
+            xMovement = -xMovement;
+    }
 
-	public void finalize() throws Throwable {
+    public Rectangle getPosition() {								// for collision analysis
+        return new Rectangle(x, y, width, height);
+    }
 
-	}
-	public void angle(){
-
-	}
-
-	public void collide(){
-
-	}
-
-	public void move(){
-
-	}
-
-	public void reset(){
-
-	}
-
-	public void size(){
-
-	}
-
-	public void speed(){
-
-	}
-}//end Ball
+    public void paint(Graphics g) {									//displays the stuff
+        g.fillRect(x, y, width, height);
+    }
+}
