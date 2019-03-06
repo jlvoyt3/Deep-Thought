1b06c0a19a929e5a46b1e99f82efb159d1d49e97
Justin Andrews <47611641+ERAUJustinAndrews@users.noreply.github.com>
2019-03-05 19:48:38 -0500

    Add files via upload

---
+++ Algm1.java
@@ -0,0 +1,32 @@
+
+
+/**
+ * @author willd
+ * @version 1.0
+ * @created 13-Feb-2019 4:08:00 PM
+ */
+public class Algm1 {
+
+	public void IterationTracker() {
+		
+	}
+
+	public Algm1(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void learning(){
+
+	}
+
+	public void saving(){
+
+	}
+
+	public void SlightVersions(){
+
+	}
+}//end Algm 1
