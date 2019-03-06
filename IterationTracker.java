ce2b22b32b351133b1943065b86610a0b13f77c7
Justin Andrews <47611641+ERAUJustinAndrews@users.noreply.github.com>
2019-03-05 19:49:16 -0500

    Add files via upload

---
+++ IterationTracker.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * @author willd
+ * @version 1.0
+ * @created 13-Feb-2019 4:08:00 PM
+ */
+public class IterationTracker {
+
+	public IterationTracker(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void counter(){
+
+	}
+
+	public void ranking(){
+
+	}
+}//end Iteration Tracker
