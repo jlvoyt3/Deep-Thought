a70b015fd5cb2e40bbae2c836453afc7208608a8
ERAU2429363 <47841126+ERAU2429363@users.noreply.github.com>
2019-03-05 19:12:21 -0500

    Complete Paddle
    Should have completed the paddle

--- Paddle.java
+++ Paddle.java
@@ -1,35 +1,46 @@
-package UML_Diagram;
+import java.awt.Rectangle;
+import java.awt.Graphics;
 
 
-/**
- * @author willd
- * @version 1.0
- * @created 13-Feb-2019 4:08:00 PM
- */
 public class Paddle {
+    private static final int width = 15, height = 125;
+    private Pong game;
+    private int upMove, downMove, x, y, yMovement;
 
-	public Manager m_Manager;
+    public Paddle(Pong game, int up, int down, int x) {
+        this.game = game;
+        this.x = x;
+        y = game.getHeight() / 2;
+        this.upMove = up;
+        this.downMove = down;
+    }
 
-	public Paddle(){
+    public void update() {
+        if (y > 0 && y < game.getHeight() - height - 40)
+            y += yMovement;
+        else if (y == 0)
+            y++;
+        else if (y == game.getHeight() - height - 30)
+            y--;
+    }
 
-	}
+    public void pressed(int keyCode) {			
+        if (keyCode == upMove)
+            yMovement = -2;
+        else if (keyCode == downMove)
+            yMovement = 2;
+    }
 
-	public void finalize() throws Throwable {
+    public void released(int keyCode) {			// player non-movement
+        if (keyCode == upMove || keyCode == downMove)
+            yMovement = 0;
+    }
 
-	}
-	public void collision(){
+    public Rectangle getBounds() {				// for collision
+        return new Rectangle(x, y, width, height);
+    }
 
-	}
-
-	public void movement(){
-
-	}
-
-	public void size(){
-
-	}
-
-	public void speed(){
-
-	}
-}//end Paddle
+    public void paint(Graphics g) {				// displays the stuff
+        g.fillRect(x, y, width, height);
+    }
+}
