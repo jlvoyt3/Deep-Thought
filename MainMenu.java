e0a5549b6dcdcc73e09bb2963765665a79841038
haileydenys <47611666+haileydenys@users.noreply.github.com>
2019-04-10 17:01:45 -0400

    Add files via upload

--- MainMenu.java
+++ MainMenu.java
@@ -1,8 +1,10 @@
+package thepackage;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.KeyCode;
@@ -24,7 +26,11 @@ import javafx.util.Duration;
  */
 public class MainMenu extends Application {
 
+	//change size of panes
+
 	//
+
+
 	Rectangle player1,player2,lowerBorder,lowerColor;
 	Circle ball;
 	Text score1;
@@ -32,7 +38,7 @@ public class MainMenu extends Application {
 	int WIDTH = 1000, HEIGHT = 400;
 	int speedX = 10, speedY = 10, dx = speedX, dy = speedY, scorePlayer1 = 0, scorePlayer2 = 0, winner = 0;
 	//
-	
+
 	/**
 	 * create the stage for the main menu
 	 */
@@ -42,7 +48,7 @@ public class MainMenu extends Application {
 	 */
 	Pane mainPane = new Pane();
 	Scene mainScene = new Scene(mainPane, 1000, 600);
-	
+
 	Pane optionsPane = new Pane();
 
 
@@ -144,17 +150,27 @@ public class MainMenu extends Application {
 		 * create and format combo box/drop down box to choose game type
 		 */
 		final ComboBox<String> pickPlayers = new ComboBox<String>();
-		pickPlayers.getItems().addAll("Player VS AI","Player VS Player","AI VS AI");
+		pickPlayers.getItems().addAll(
+				"Player VS Player",
+				"Player VS Traditional AI",
+				"Player VS Deep Learning AI"
+				);
 		pickPlayers.setLayoutX(50);
 		pickPlayers.setLayoutY(300);
 		pickPlayers.setPromptText("Choose Game Type");
+		
+
 
 
 		/**
 		 * create and format combo box/drop down box to choose difficulty
 		 */
 		final ComboBox<String> pickDifficulty = new ComboBox<String>();
-		pickDifficulty.getItems().addAll("Easy","Medium","Hard");
+		pickDifficulty.getItems().addAll(
+				"Easy",
+				"Medium",
+				"Hard"
+				);
 		pickDifficulty.setLayoutX(250);
 		pickDifficulty.setLayoutY(300);
 		pickDifficulty.setPromptText("Choose Difficulty");
@@ -162,11 +178,7 @@ public class MainMenu extends Application {
 		/**
 		 * create and format combo box/drop down box to choose AI
 		 */
-		final ComboBox<String> pickAI = new ComboBox<String>();
-		pickAI.getItems().addAll("AI Jerry","AI Fred","AI Chad");
-		pickAI.setLayoutX(450);
-		pickAI.setLayoutY(300);
-		pickAI.setPromptText("Choose AI");
+
 
 		/**
 		 * set action for newGameButton
@@ -176,7 +188,7 @@ public class MainMenu extends Application {
 			
 			mainPane.getChildren().removeAll(startButton, helpButton, scoreButton, text);
 
-			mainPane.getChildren().addAll(pickPlayers, pickDifficulty, pickAI, title, startGameButton);
+			mainPane.getChildren().addAll(pickPlayers, pickDifficulty, title, startGameButton);
 		});
 
 		/**
@@ -196,166 +208,188 @@ public class MainMenu extends Application {
 		/**
 		 * set action of startButton to display gameScene
 		 */
+
 		startGameButton.setOnAction(e->{
-			//ballPaddles.createGame();
-			//
-			mainPane.getChildren().removeAll(pickPlayers, pickDifficulty, pickAI, title, startGameButton);
-			//stage.show();
-			mainScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-				public void handle(KeyEvent ke) {
-					if (ke.getCode() == KeyCode.W && player1.getLayoutY() > 1) {
-						player1.setLayoutY(player1.getLayoutY() - 30);
-					}
-					if (ke.getCode() == KeyCode.S && player1.getLayoutY() < 320) {
-						player1.setLayoutY(player1.getLayoutY() + 30);
-					}
-					if (ke.getCode() == KeyCode.UP && player2.getLayoutY() > 1) {
-						player2.setLayoutY(player2.getLayoutY() - 30);
-					}
-					if (ke.getCode() == KeyCode.DOWN  && player2.getLayoutY() < 320) {
-						player2.setLayoutY(player2.getLayoutY() + 30);
-					}
+
+			if ( pickPlayers.getValue() == null || pickDifficulty.getValue() == null) {
+				
+				AlertBox.display("Error", "You must set your options before the game begins.");
+				
+			} else {
+
+				
+				int playStyle = 0;
+				
+				if (pickPlayers.getValue() == "Player VS Player") {
+					playStyle = 1;
+					System.out.println(playStyle);
+				} else if (pickPlayers.getValue() == "Player VS Traditional AI") {
+					playStyle = 2;
+					System.out.println(playStyle);
+				} else if (pickPlayers.getValue() == "Player VS Deep Learning AI") {
+					playStyle = 3;
+					System.out.println(playStyle);
 				}
-			});
+					
+				
+				
+				mainPane.getChildren().removeAll(pickPlayers, pickDifficulty, title, startGameButton);
 
-			mainPane.setMaxSize(WIDTH, HEIGHT);
-			mainPane.setStyle("-fx-background-color: #86DB64");
-			
-			
-			lowerColor = new Rectangle(1000,200, Color.GREEN);
-			lowerColor.setLayoutX(0);
-			lowerColor.setLayoutY(405);
-			
-			lowerBorder = new Rectangle(1000,10, Color.BLACK);
-			lowerBorder.setLayoutX(0);
-			lowerBorder.setLayoutY(400);
+				mainScene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+					public void handle(KeyEvent ke) {
+						if (ke.getCode() == KeyCode.W && player1.getLayoutY() > 1) {
+							player1.setLayoutY(player1.getLayoutY() - 30);
+						}
+						if (ke.getCode() == KeyCode.S && player1.getLayoutY() < 320) {
+							player1.setLayoutY(player1.getLayoutY() + 30);
+						}
+						if (ke.getCode() == KeyCode.UP && player2.getLayoutY() > 1) {
+							player2.setLayoutY(player2.getLayoutY() - 30);
+						}
+						if (ke.getCode() == KeyCode.DOWN  && player2.getLayoutY() < 320) {
+							player2.setLayoutY(player2.getLayoutY() + 30);
+						}
+					}
+				});
 
-			player1 = new Rectangle(10,80, Color.BLACK);
-			player1.setLayoutX(0);
-			player1.setLayoutY(HEIGHT/2-40);
+				mainPane.setMaxSize(WIDTH, HEIGHT);
+				mainPane.setStyle("-fx-background-color: #86DB64");
 
-			player2 = new Rectangle(10,80, Color.BLACK);
-			player2.setLayoutX(WIDTH-10);
-			player2.setLayoutY(HEIGHT/2-40);
 
-			ball = new Circle(5);
-			ball.setFill(Color.BLACK);
-			ball.setLayoutX(WIDTH/2);
-			ball.setLayoutY(HEIGHT/2);
+				lowerColor = new Rectangle(1000,200, Color.GREEN);
+				lowerColor.setLayoutX(0);
+				lowerColor.setLayoutY(405);
 
-			score1 = new Text();
-			score1.setLayoutX(300);
-			score1.setLayoutY(300);
-			score1.setText("0");
+				lowerBorder = new Rectangle(1000,10, Color.BLACK);
+				lowerBorder.setLayoutX(0);
+				lowerBorder.setLayoutY(400);
 
-			score2 = new Text();
-			score2.setLayoutX(800);
-			score2.setLayoutY(300);
-			score2.setText("0");
+				player1 = new Rectangle(10,80, Color.BLACK);
+				player1.setLayoutX(0);
+				player1.setLayoutY(HEIGHT/2-40);
 
-			mainPane.getChildren().addAll(player1,player2,ball,score1,score2,lowerBorder,lowerColor);
+				player2 = new Rectangle(10,80, Color.BLACK);
+				player2.setLayoutX(WIDTH-10);
+				player2.setLayoutY(HEIGHT/2-40);
 
-			Timeline timelineGame = new Timeline();
-			timelineGame.setCycleCount(Timeline.INDEFINITE);
-			KeyFrame keyframeTargets = new KeyFrame(Duration.seconds(.08), action -> {
+				ball = new Circle(5);
+				ball.setFill(Color.BLACK);
+				ball.setLayoutX(WIDTH/2);
+				ball.setLayoutY(HEIGHT/2);
+
+				score1 = new Text();
+				score1.setLayoutX(300);
+				score1.setLayoutY(300);
+				score1.setText("0");
+
+				score2 = new Text();
+				score2.setLayoutX(800);
+				score2.setLayoutY(300);
+				score2.setText("0");
+
+				mainPane.getChildren().addAll(player1,player2,ball,score1,score2,lowerBorder,lowerColor);
+
+				Timeline timelineGame = new Timeline();
+				timelineGame.setCycleCount(Timeline.INDEFINITE);
+				KeyFrame keyframeTargets = new KeyFrame(Duration.seconds(.08), action -> {
+					//
+					//gameUpdate
+					double x = ball.getLayoutX(), y = ball.getLayoutY();
+
+					if (x <=10 && x > -10 && y > player1.getLayoutY() && y < player1.getLayoutY()+80) {
+						dx = speedX;
+					} else if (ball.getLayoutX() < -10) {
+						player1.setLayoutY(HEIGHT/2-40);
+						player2.setLayoutY(HEIGHT/2-40);
+						ball.setLayoutX(WIDTH/2);
+						ball.setLayoutY(HEIGHT/2);
+						speedX = 10;
+						speedY = 10;
+						scorePlayer2++;
+
+						//remove old score
+						mainPane.getChildren().remove(score2);
+						//convert score to string
+						String scorePlayer2string = Integer.toString(scorePlayer2);
+						//set text to score
+						score2.setText(scorePlayer2string);
+						//add score to pane
+						mainPane.getChildren().add(score2);
+					}
+
+					if(x >= WIDTH-10 && x < WIDTH+10 && y > player2.getLayoutY() && y< player2.getLayoutY()+80) {
+						speedX++;
+						dx = -speedX;
+					} else if (ball.getLayoutX() > 1010) {
+						player1.setLayoutY(HEIGHT/2-40);
+						player2.setLayoutY(HEIGHT/2-40);
+						ball.setLayoutX(WIDTH/2);
+						ball.setLayoutY(HEIGHT/2);
+						speedX = 10;
+						speedY = 10;
+						scorePlayer1++;
+
+						//remove old score
+						mainPane.getChildren().remove(score1);
+						//convert score to string
+						String scorePlayer1string = Integer.toString(scorePlayer1);
+						//set text to score
+						score1.setText(scorePlayer1string);
+						//add score to pane
+						mainPane.getChildren().add(score1);
+					}
+
+					if(y <= 0) {
+						dy = speedY;
+					}
+
+					if (y >= HEIGHT-5) {
+						dy = -speedY;
+					}
+
+					//end game
+					if (scorePlayer1 == 10) {
+						player1.setLayoutY(HEIGHT/2-40);
+						player2.setLayoutY(HEIGHT/2-40);
+						ball.setLayoutX(WIDTH/2);
+						ball.setLayoutY(HEIGHT/2);
+						speedX = 0;
+						speedY = 0;
+						mainPane.getChildren().removeAll(ball, score1, score2, player1, player2);
+						Text win1 = new Text();
+						win1.setLayoutX(450);
+						win1.setWrappingWidth(100);
+						win1.setLayoutY(200);
+						win1.setText("Player 1 wins!");
+						mainPane.getChildren().add(win1);
+						winner = 1;
+					}
+					if (scorePlayer2 == 10) {
+						player1.setLayoutY(HEIGHT/2-40);
+						player2.setLayoutY(HEIGHT/2-40);
+						ball.setLayoutX(WIDTH/2);
+						ball.setLayoutY(HEIGHT/2);
+						speedX = 0;
+						speedY = 0;
+						mainPane.getChildren().removeAll(ball, score1, score2, player1, player2);
+						Text win2 = new Text();
+						win2.setLayoutX(450);
+						win2.setWrappingWidth(100);
+						win2.setLayoutY(200);
+						win2.setText("Player 2 wins!");
+						mainPane.getChildren().add(win2);
+						winner = 2;
+					}
+
+					ball.setLayoutX(ball.getLayoutX()+dx);
+					ball.setLayoutY(ball.getLayoutY()+dy);
+					//
+					//
+				});
+				timelineGame.getKeyFrames().add(keyframeTargets);
+				timelineGame.play();
 				//
-				//gameUpdate
-				double x = ball.getLayoutX(), y = ball.getLayoutY();
-
-				if (x <=10 && x > -10 && y > player1.getLayoutY() && y < player1.getLayoutY()+80) {
-					dx = speedX;
-				} else if (ball.getLayoutX() < -10) {
-					player1.setLayoutY(HEIGHT/2-40);
-					player2.setLayoutY(HEIGHT/2-40);
-					ball.setLayoutX(WIDTH/2);
-					ball.setLayoutY(HEIGHT/2);
-					speedX = 10;
-					speedY = 10;
-					scorePlayer2++;
-
-					//remove old score
-					mainPane.getChildren().remove(score2);
-					//convert score to string
-					String scorePlayer2string = Integer.toString(scorePlayer2);
-					//set text to score
-					score2.setText(scorePlayer2string);
-					//add score to pane
-					mainPane.getChildren().add(score2);
-				}
-
-				if(x >= WIDTH-10 && x < WIDTH+10 && y > player2.getLayoutY() && y< player2.getLayoutY()+80) {
-					speedX++;
-					dx = -speedX;
-				} else if (ball.getLayoutX() > 1010) {
-					player1.setLayoutY(HEIGHT/2-40);
-					player2.setLayoutY(HEIGHT/2-40);
-					ball.setLayoutX(WIDTH/2);
-					ball.setLayoutY(HEIGHT/2);
-					speedX = 10;
-					speedY = 10;
-					scorePlayer1++;
-
-					//remove old score
-					mainPane.getChildren().remove(score1);
-					//convert score to string
-					String scorePlayer1string = Integer.toString(scorePlayer1);
-					//set text to score
-					score1.setText(scorePlayer1string);
-					//add score to pane
-					mainPane.getChildren().add(score1);
-				}
-
-				if(y <= 0) {
-					dy = speedY;
-				}
-
-				if (y >= HEIGHT-5) {
-					dy = -speedY;
-				}
-
-				//end game
-				if (scorePlayer1 == 10) {
-					player1.setLayoutY(HEIGHT/2-40);
-					player2.setLayoutY(HEIGHT/2-40);
-					ball.setLayoutX(WIDTH/2);
-					ball.setLayoutY(HEIGHT/2);
-					speedX = 0;
-					speedY = 0;
-					mainPane.getChildren().removeAll(ball, score1, score2, player1, player2);
-					Text win1 = new Text();
-					win1.setLayoutX(450);
-					win1.setWrappingWidth(100);
-					win1.setLayoutY(200);
-					win1.setText("Player 1 wins!");
-					mainPane.getChildren().add(win1);
-					winner = 1;
-				}
-				if (scorePlayer2 == 10) {
-					player1.setLayoutY(HEIGHT/2-40);
-					player2.setLayoutY(HEIGHT/2-40);
-					ball.setLayoutX(WIDTH/2);
-					ball.setLayoutY(HEIGHT/2);
-					speedX = 0;
-					speedY = 0;
-					mainPane.getChildren().removeAll(ball, score1, score2, player1, player2);
-					Text win2 = new Text();
-					win2.setLayoutX(450);
-					win2.setWrappingWidth(100);
-					win2.setLayoutY(200);
-					win2.setText("Player 2 wins!");
-					mainPane.getChildren().add(win2);
-					winner = 2;
-				}
-
-				ball.setLayoutX(ball.getLayoutX()+dx);
-				ball.setLayoutY(ball.getLayoutY()+dy);
-				//
-				//
-			});
-			timelineGame.getKeyFrames().add(keyframeTargets);
-			timelineGame.play();
-			//
-		});
+			}});
 
 		//backButton.setOnAction(e->{
 		//	stage.setScene(optionScene);
