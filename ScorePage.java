15ce0bc3ad06b5ed5a5423ec32d2899b767f58ea
haileydenys <47611666+haileydenys@users.noreply.github.com>
2019-04-17 16:47:14 -0400

    Add files via upload

--- ScorePage.java
+++ ScorePage.java
@@ -1,4 +1,25 @@
+package thepackage;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
 public class ScorePage {
 
+	public ScorePage() {
+		
+		Stage scoreStage = new Stage();
+		Pane scorePane = new Pane();
+		Scene scoreScene = new Scene(scorePane, 700, 700);
+		
+		
+		
+		
+		scorePane.setStyle("-fx-background: #16A500");
+		scorePane.getChildren().addAll();
+		scoreStage.setScene(scoreScene);
+		scoreStage.show();
+		
+	}
+	
 }
