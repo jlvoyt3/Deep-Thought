ae7d898bafb382989134171ea05f72471a22abc6
haileydenys <47611666+haileydenys@users.noreply.github.com>
2019-04-24 12:12:26 -0400

    Delete TradAI.java

--- TradAI.java
+++
@@ -1,7 +0,0 @@
-if (ball.getLayoutY() > player2.getLayoutY()) {
-	player2.setLayoutY(ball.getLayoutY() + 5);
-}
-else (ball.getLayoutY() < player2.getLayoutY()) {
-	player2.setLayoutY(ball.getLayoutY() - 5);
-}
-	
